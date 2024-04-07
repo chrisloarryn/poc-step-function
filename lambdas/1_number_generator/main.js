3ad1a0b672ba7ba51f8dcaab77d8ad1bb4ee1608
EC2 Default User <ec2-user@ip-172-31-28-135.us-west-2.compute.internal>
2024-04-07 15:49:48 +0000

    hello from c9

--- lambdas/1_number_generator/main.js
+++ lambdas/1_number_generator/main.js
@@ -2,6 +2,8 @@
 exports.handler = async event => {
   var number = Math.floor(Math.random() * 100) + 1;
   console.log(`El numero generado es: ${number}`);
+  
+  console.log(`Hello world the number is ${number}`;
 
-  return {number: number, is_even: number % 2 === 0}
+  return {number: number, is_even: number % 2 === 0};
 };
